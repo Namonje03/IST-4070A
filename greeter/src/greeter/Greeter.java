910ee943a0fb90aedcaff79e3a9e3a70d237b7a6
Esther152 <esther@codesavanna.org>
2025-09-22 08:32:47 +0300

    add greeter and variables projects

---
+++ greeter/src/greeter/Greeter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package greeter;
+
+/**
+ *
+ * @author Esther Namonje
+ */
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");    
+    }  
+    
+    public static void main(String[] args) {
+        (new Greeter()).greet();
+        // TODO
+    }
+    
+}
